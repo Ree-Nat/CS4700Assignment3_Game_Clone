9d74fffd5d6116d564cef6e3d307429e17ce25ca
Nathan <53132424+Ree-Nat@users.noreply.github.com>
2026-03-03 09:40:12 -0800

    instantiate enemy

---
+++ CS4700_Assignment3/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+/***************************************************************
+*file: DoubleIt.cs
+*author: Nathan
+*class: CS 4700 ? Game Development
+*assignment: program 1
+*date last modified: 3/3/2022
+*
+*purpose: This program controls enemy's behavior and movment 
+*
+****************************************************************/
+
+public class EnemyController : MonoBehaviour
+{
+
+    Collider2D enemy_hurtbox;
+    Collider2D enemy_hitbox ;
+
+    Collider2D playerHurtbox;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
