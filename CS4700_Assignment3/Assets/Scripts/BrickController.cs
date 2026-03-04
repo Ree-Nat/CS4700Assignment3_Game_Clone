471911c60d44088a9bf2a45561231024e5b0c1f7
Nathan <53132424+Ree-Nat@users.noreply.github.com>
2026-03-04 13:12:18 -0800

    implement graphics with brick asset

---
+++ CS4700_Assignment3/Assets/Scripts/BrickController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class BrickController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+        
+    //Breaks when player hurtbox hit object
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "PlayerHurtBox")
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
--- CS4700_Assignment3/Assets/Scripts/EnemyController.cs
+++ CS4700_Assignment3/Assets/Scripts/EnemyController.cs
@@ -28,7 +28,6 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print("test");
         moveLeft();
     }
 
