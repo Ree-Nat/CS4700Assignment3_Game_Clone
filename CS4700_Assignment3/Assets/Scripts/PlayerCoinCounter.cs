6ac0960b8d87245b738baf1757eab4cfdc411421
jacobtakaoka <jaketaka@hotmail.com>
2026-03-10 13:30:24 -0700

    added comments/desc

--- CS4700_Assignment3/Assets/Scripts/PlayerCoinCounter.cs
+++ CS4700_Assignment3/Assets/Scripts/PlayerCoinCounter.cs
@@ -1,5 +1,13 @@
 using UnityEngine;
-
+/***************************************************************
+*file: PlayerCoinCounter.cs
+*author: T. Diaz
+*class: CS 4700 - Game Development
+*assignment: program 3
+*date last modified: 3/10/2026
+*
+*purpose: This program iterates the total coin amount and prints total to console when player collects coin.
+****************************************************************/
 public class PlayerCoinCounter : MonoBehaviour
 {
     public int coins = 0;
